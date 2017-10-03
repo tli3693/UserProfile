573ab930c038ff700df3bf65d46ad5de3799c49e
Troy <tli3693@gmail.com>
2017-10-02 20:20:31 -0400

    Part 6

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # UserProfile
 Using MEAN Stack
 
-#Angular Commands Used
+# Some dependencies
+Backend
+express
+mongoose
+bcryptjs
+cors
+jsonwebtoken
+body-parser
+passport
+passport-jwt
+
+Front-End
+angular-cli
+angular2-flash-messages
+
+# Angular Commands Used
+nodemon
+
 npm install -g @angular/cli
 ng new angular-src
 cd angular-src
 ng serve
+ng g component path/to/create/component
+ng g service path/to/create/service
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  // Inject services into constructor
+  constructor(private validateService: ValidateService, private flashMessagesService: FlashMessagesService) { 
+
+  }
 
   ngOnInit() {
+
+  }
+
+  // Applied to submit function on form
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password,
+    }
+
+    // Required Fields
+    if(!this.validateService.validateRegister(user)) {
+      this.flashMessagesService.show("Not a valid input. Please input all fields.", {cssClass: 'alert-danger', timeout: 3000});
+      console.log("Not a valid input. Please input all fields");
+      return false;
+    } else {
+      if(!this.validateService.validateEmail(user.email)) {
+        this.flashMessagesService.show("Enter a valid email.", {cssClass: 'alert-danger', timeout: 3000});
+        console.log("Enter a valid email.");
+        return false;
+      }
+    }
+    this.flashMessagesService.show("SUCCESSFUL REGISTER");
+    console.log("Successful register!");
+    
+    return true;
+
+    
   }
 
 }
