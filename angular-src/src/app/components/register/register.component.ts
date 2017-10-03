60c05ee2cb67d1ac452e08cbf6be4a250853e82a
Troy <tli3693@gmail.com>
2017-10-02 20:53:25 -0400

    Some additional validation/mesages in registration page

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -12,10 +12,14 @@ export class RegisterComponent implements OnInit {
   username: String;
   email: String;
   password: String;
-
+  errorMessage: String;
+  input = {
+    "error": false,
+    "errorMessage": ""
+  }
   // Inject services into constructor
   constructor(private validateService: ValidateService, private flashMessagesService: FlashMessagesService) { 
-
+    this.input.errorMessage = "TESTING";
   }
 
   ngOnInit() {
@@ -29,16 +33,22 @@ export class RegisterComponent implements OnInit {
       email: this.email,
       username: this.username,
       password: this.password,
+     
     }
-
+    
     // Required Fields
     if(!this.validateService.validateRegister(user)) {
-      this.flashMessagesService.show("Not a valid input. Please input all fields.", {cssClass: 'alert-danger', timeout: 3000});
+      //this.flashMessagesService.show("Not a valid input. Please input all fields.", {cssClass: 'alert-danger', timeout: 3000});
+      this.input.error = true;
+      this.input.errorMessage = "Not a valid input. Please input all fields.";
+
       console.log("Not a valid input. Please input all fields");
       return false;
     } else {
       if(!this.validateService.validateEmail(user.email)) {
-        this.flashMessagesService.show("Enter a valid email.", {cssClass: 'alert-danger', timeout: 3000});
+        //this.flashMessagesService.show("Enter a valid email.", {cssClass: 'alert-danger', timeout: 3000});
+        this.input.error = true;
+        this.input.errorMessage = "Enter a valid email.";
         console.log("Enter a valid email.");
         return false;
       }
