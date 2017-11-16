5f26a59e671ab184833aa9b0cca462856cd1fa86
Troy <tli3693@gmail.com>
2017-11-16 00:02:36 -0500

    added constant and formatting html

--- angular-src/src/app/services/task.service.ts
+++ angular-src/src/app/services/task.service.ts
@@ -3,18 +3,21 @@ import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { tokenNotExpired } from 'angular2-jwt';
 
+const rootContextPath = "http://localhost:3000";
+
 @Injectable()
 export class TaskService {
 
-  constructor(private http:Http) { }
+
+  constructor(private http: Http) { }
 
   getAllTasksByUsername(username) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
     console.log("In task.service.getAllTasksByUsername(username) function with username: " + username);
-    return this.http.post('http://localhost:3000/tasks/findAllTasks', {username: username}, {headers: headers})
-    .map(res => res.json());
+    return this.http.post(rootContextPath + '/tasks/findAllTasks', { username: username }, { headers: headers })
+      .map(res => res.json());
   }
 
   getAllStatuses() {
@@ -22,8 +25,8 @@ export class TaskService {
     headers.append('Content-Type', 'application/json');
 
     console.log("In task.service.getTaskStatus(statusCode) function");
-    return this.http.get('http://localhost:3000/tasks/getAllTaskStatuses', {headers: headers})
-    .map(res => res.json());
+    return this.http.get(rootContextPath + '/tasks/getAllTaskStatuses', { headers: headers })
+      .map(res => res.json());
   }
-  
+
 }
